06f0d3d41a59ae830b16921ae66c1ee65922da48
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-12-23 17:36:29 +0100

    Comments

--- array2d_derive/src/lib.rs
+++ array2d_derive/src/lib.rs
@@ -17,7 +17,9 @@ impl Parse for Array2DInput {
     }
 }
 
+
 /// Creates a 2D array struct with the given width and height on the stack using a single array.
+/// 
 /// # Parameters
 /// - width: usize - The width of the 2D array.
 /// - height: usize - The height of the 2D array.
@@ -31,6 +33,17 @@ pub fn create_array2d(input: TokenStream) -> TokenStream {
     let struct_name = format_ident!("Array2D_{}_{}", width, height);
 
     let expanded = quote! {
+        /// 2D array with fixed width and height.
+        /// The data is stored in a single stack-allocated array to improve cache locality and prevent overhead from indirection and heap allocation.
+        /// 
+        /// # Example
+        /// ``` rust
+        /// use array2d::create_array2d;
+        /// create_array2d!(3, 4);
+        /// 
+        /// fn main() {
+        ///     let array = Array2D_3_4::default(0u16);
+        /// }
         pub struct #struct_name<T> {
             pub data: [T; #len],
         }
@@ -46,7 +59,18 @@ pub fn create_array2d(input: TokenStream) -> TokenStream {
                 #height
             }
 
+            /// Returns the total number of elements in the 2D array.
+            pub fn len(&self) -> usize {
+                #len
+            }
+
             /// Creates a new 2D array with the given data. 
+            /// 
+            /// # Parameters
+            /// - data: [T; #len] - The data to initialize the 2D array with. 
+            /// 
+            /// # Returns
+            /// - Self - The new 2D array.
             pub fn new(data: [T; #len]) -> Self {
                 #struct_name {
                     data,
@@ -54,15 +78,31 @@ pub fn create_array2d(input: TokenStream) -> TokenStream {
             }
 
             /// Creates a new 2D array with the given default value.
-            pub fn default(value: T) -> Self
+            /// 
+            /// # Parameters
+            /// - default: T - The default value to initialize the 2D array with.
+            /// 
+            /// # Returns
+            /// - Self - The new 2D array.
+            pub fn default(default: T) -> Self
             where
                 T: Copy,
             {
                 #struct_name {
-                    data: [value; #len],
+                    data: [default; #len],
                 }
             }
 
+            /// Returns the element at the given x and y coordinates or None if the coordinates are out of bounds.
+            /// 
+            /// # Parameters
+            /// - x: usize - The x coordinate.
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - Option<&T> - A reference to the element at the given coordinates or None if the coordinates are out of bounds.
+            /// 
+            /// # Example
             pub fn get(&self, x: usize, y: usize) -> Option<&T> {
                 if x < #width && y < #height {
                     Some(&self.data[y * #width + x])
@@ -71,6 +111,14 @@ pub fn create_array2d(input: TokenStream) -> TokenStream {
                 }
             }
 
+            /// Returns a mutable reference to the element at the given x and y coordinates or None if the coordinates are out of bounds.
+            /// 
+            /// # Parameters
+            /// - x: usize - The x coordinate.
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - Option<&mut T> - A mutable reference to the element at the given coordinates or None if the coordinates are out of bounds.
             pub fn get_mut(&mut self, x: usize, y: usize) -> Option<&mut T> {
                 if x < #width && y < #height {
                     Some(&mut self.data[y * #width + x])
@@ -79,35 +127,88 @@ pub fn create_array2d(input: TokenStream) -> TokenStream {
                 }
             }
 
-            pub fn set(&mut self, x: usize, y: usize, value: T) -> Option<()> {
+            /// Sets the element at the given x and y coordinates to the given value and returns true if the coordinates are in bounds.
+            /// 
+            /// # Parameters
+            /// - x: usize - The x coordinate.
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - bool - True if the coordinates are in bounds, false otherwise.
+            pub fn set(&mut self, x: usize, y: usize, value: T) -> bool {
                 if x < #width && y < #height {
                     self.data[y * #width + x] = value;
-                    Some(())
-                } else {
-                    None
+                    return true;
                 }
+                false
             }
 
+            /// Sets the element at the given x and y coordinates to the given value without checking bounds.
+            /// 
+            /// # Safety
+            /// This method can panic or modify wrong cells if the coordinates are out of bounds.
+            /// 
+            /// # Parameters
+            /// - x: usize - The x coordinate.
+            /// - y: usize - The y coordinate.
+            pub fn set_unchecked(&mut self, x: usize, y: usize, value: T) {
+                self.data[y * #width + x] = value;
+            }
+
+            /// Returns the element at the given x and y coordinates without checking bounds.
+            /// 
+            /// # Safety
+            /// This method can panic or modify wrong cells if the coordinates are out of bounds.
+            /// 
+            /// # Parameters
+            /// - x: usize - The x coordinate.
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - &T - A reference to the element at the given coordinates.
             fn get_unchecked(&self, x: usize, y: usize) -> &T {
                 &self.data[y * #width + x]
             }
 
+            /// Iterates over the 2D array row by row.
             pub fn iter(&self) -> impl Iterator<Item = &T> {
                 self.data.iter()
             }
 
+            /// Iterates over the 2D array row by row mutably.
             pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut T> {
                 self.data.iter_mut()
             }
 
+            /// Iterates over the row at the given y coordinate.
+            /// 
+            /// # Parameters
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - impl Iterator<Item = &T> - An iterator over the row at the given y coordinate.
             pub fn iter_row(&self, y: usize) -> impl Iterator<Item = &T> {
                 self.data[y * #width..(y + 1) * #width].iter()
             }
 
+            /// Iterates over the row at the given y coordinate mutably.
+            /// 
+            /// # Parameters
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - impl Iterator<Item = &mut T> - An iterator over the row at the given y coordinate mutably.
             pub fn iter_row_mut(&mut self, y: usize) -> impl Iterator<Item = &mut T> {
                 self.data[y * #width..(y + 1) * #width].iter_mut()
             }
 
+            /// Returns the row at the given y coordinate or None if the coordinate is out of bounds.
+            /// 
+            /// # Parameters
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - Option<&[T]> - A reference to the row at the given y coordinate or None if the coordinate is out of bounds.
             pub fn get_row(&self, y: usize) -> Option<&[T]> {
                 if y < #height {
                     Some(&self.data[y * #width..(y + 1) * #width])
@@ -116,6 +217,13 @@ pub fn create_array2d(input: TokenStream) -> TokenStream {
                 }
             }
 
+            /// Returns the row at the given y coordinate mutably or None if the coordinate is out of bounds.
+            /// 
+            /// # Parameters
+            /// - y: usize - The y coordinate.
+            /// 
+            /// # Returns
+            /// - Option<&mut [T]> - A mutable reference to the row at the given y coordinate or None if the coordinate is out of bounds.
             pub fn get_row_mut(&mut self, y: usize) -> Option<&mut [T]> {
                 if y < #height {
                     Some(&mut self.data[y * #width..(y + 1) * #width])
@@ -143,9 +251,11 @@ pub fn create_array2d(input: TokenStream) -> TokenStream {
             T: std::fmt::Debug,
         {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                let max_entry_width = self.data.iter().map(|entry| format!("{:?}", entry).len()).max().unwrap_or(0);
+
                 for y in 0..#height {
                     for x in 0..#width {
-                        write!(f, "{:?} ", self.get(x, y).unwrap())?;
+                        write!(f, "{:>width$} ", format!("{:?}", self.get(x, y).unwrap()), width = max_entry_width)?;
                     }
                     writeln!(f)?;
                 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use array2d::create_array2d;
 
-create_array2d!(2,3);
-create_array2d!(10,20);
+create_array2d!(5,8);
+create_array2d!(50,80);
 
 fn main() {
-    let mut array = Array2D_2_3::default(0);
+    let mut array = Array2D_5_8::default(0u16);
 
-    array.set(1, 2, 1);
-    assert_eq!(array.get(1, 2), Some(&1));
+    array.set_unchecked(1, 1, 1);
+    array.set_unchecked(2, 2, 2);
+
+    let array_big = Array2D_50_80::default(0u64);
+    
+    println!("{:?}", array);
+
 }
